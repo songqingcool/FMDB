72120d00545a4d87426b91054e577a6c8664b18a
songqinggong <songqinggong@syswin.com>
2016-09-27 16:31:42 +0800

    FMDB工程配置

---
+++ FMDB/FMDB/FMDB.h
@@ -0,0 +1,19 @@
+//
+//  FMDB.h
+//  FMDB
+//
+//  Created by SONGQG on 2016/9/27.
+//  Copyright © 2016年 思源. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FMDB.
+FOUNDATION_EXPORT double FMDBVersionNumber;
+
+//! Project version string for FMDB.
+FOUNDATION_EXPORT const unsigned char FMDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FMDB/PublicHeader.h>
+
+
